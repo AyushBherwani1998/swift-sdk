4b5acafd9009cfd50f69f4e2de447d4d1af0df36
Pawann Kumaarr <mtechviral@gmail.com>
2022-06-02 14:09:23 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.4.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.8.2"),
-        .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.0"),
+        .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.4"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.3.0"),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
         .package(url: "https://github.com/malcommac/SwiftDate", from: "6.0.1"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.4.0"),
         .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.4"),
-        .package(url: "https://github.com/mghnmtt/HDWalletKit-1.git", from: "0.3.8"),
+        .package(url: "https://github.com/iampawan/HDWalletKit-1.git", from: "0.3.8"),
     ],
     targets: [
         .target(name: "binancechain",
